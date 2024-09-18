dd88c124ee8f289fe0b8616a8d7c13fb6ae459e9
Evan <>
2024-09-17 22:06:03 -0700

    fixed cogs

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ import os
 
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
-import cogs
+from cogs.connect4 import Connect4
+from cogs.pkmncards import PkmnCards
+from cogs.poke2spy import Poke2Spy
+from cogs.tetris import Tetris
+from cogs.wordle import Wordle
 
 load_dotenv()
 
@@ -17,11 +20,11 @@ bot = commands.Bot(
 
 @bot.event
 async def on_ready():
-    await bot.add_cog(cogs.PkmnCards())
-    await bot.add_cog(cogs.Poke2Spy())
-    await bot.add_cog(cogs.Wordle(bot))
-    await bot.add_cog(cogs.Connect4(bot))
-    await bot.add_cog(cogs.Tetris(bot))
+    await bot.add_cog(PkmnCards())
+    await bot.add_cog(Poke2Spy())
+    await bot.add_cog(Wordle(bot))
+    await bot.add_cog(Connect4(bot))
+    await bot.add_cog(Tetris(bot))
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
