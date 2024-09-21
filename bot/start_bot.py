fc38420ce73c49bea2af61c371412daee45e1a5f
Evan <>
2024-09-20 19:46:34 -0700

    fix precommit hook

--- bot/start_bot.py
+++ bot/start_bot.py
@@ -1,14 +1,13 @@
 import os
-from discord.ext import commands
-from dotenv import load_dotenv
-from cogs.tetris import Tetris
-from cogs.poke2spy import Poke2Spy
-import discord
 
+import discord
 from cogs.connect4 import Connect4
 from cogs.pkmncards import PkmnCards
+from cogs.poke2spy import Poke2Spy
+from cogs.tetris import Tetris
 from cogs.wordle import Wordle
-
+from discord.ext import commands
+from dotenv import load_dotenv
 
 load_dotenv()
 has_loaded_cogs = False
