79ef6458319298c6deaa180c1413df721e02dd5e
Evan <>
2024-09-20 19:40:16 -0700

    added ruff formating prehook

--- bot/start_bot.py
+++ bot/start_bot.py
@@ -1,15 +1,17 @@
 import os
-
-import discord
 from cogs.connect4 import Connect4
 from cogs.pkmncards import PkmnCards
 from cogs.poke2spy import Poke2Spy
-from cogs.tetris import Tetris
+import discord
+
 from cogs.wordle import Wordle
 from discord.ext import commands
 from dotenv import load_dotenv
+from cogs.tetris import Tetris
+
 
 load_dotenv()
+has_loaded_cogs = False
 
 bot = commands.Bot(
     command_prefix="poketcg ",
@@ -20,11 +22,17 @@ bot = commands.Bot(
 
 @bot.event
 async def on_ready():
+    global has_loaded_cogs
+    if has_loaded_cogs:
+        return
+
     await bot.add_cog(PkmnCards())
     await bot.add_cog(Poke2Spy())
     await bot.add_cog(Wordle(bot))
     await bot.add_cog(Connect4(bot))
     await bot.add_cog(Tetris(bot))
+
+    has_loaded_cogs = True
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
